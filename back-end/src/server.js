52dc244f1a2e8315a95ece17e3cb6bc98397a8cb
Lindokuhle Nsibande <lindokuhle@rokkit.co.za>
2025-07-19 09:39:09 +0200

    done with setting up an express server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hellow', function(req, res){
-    res.send('Hellow!');
+    res.send('Hellow from get request!');
+});
+
+app.post('/hellow', function(req, res){
+    res.send('Hellow, '+ req.body.name +' from get request!');
 });
 
 app.listen(8000, function(){
